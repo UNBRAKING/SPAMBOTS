4172c1ecd44c44467b420928b085b1672f8e6148
UNBRAKING THINGS <code.pbail@gmail.com>
2024-10-26 17:33:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,14 +10,14 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 # VALUES REQUIRED FOR XBOTS
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 26614080
+API_HASH = "7d2c9a5628814e1430b30a1f0dc0165b"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
-BOT_TOKEN = getenv("BOT_TOKEN", default=None)
-BOT_TOKEN2 = getenv("BOT_TOKEN2", default=None)
+BOT_TOKEN = getenv("7562440740:AAFJxxxjXHWm7BDmnsbCfTsZfAD3s9UgiIY", default=None)
+BOT_TOKEN2 = getenv("7512613727:AAE4bmjZuGDEOZvUP_rIOYxz1yLEle6eEUo", default=None)
 BOT_TOKEN3 = getenv("BOT_TOKEN3", default=None)
 BOT_TOKEN4 = getenv("BOT_TOKEN4", default=None)
 BOT_TOKEN5 = getenv("BOT_TOKEN5", default=None)
@@ -27,10 +27,10 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = getenv("BOT_TOKEN10", default=None)
 
-SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5518687442").split()))
+SUDO_USERS = list(map(lambda x: int(x), getenv("SUDO_USERS", default="5606990991").split()))
 for x in OXYGEN:
     SUDO_USERS.append(x)
-OWNER_ID = int(getenv("OWNER_ID", default="5059737154"))
+OWNER_ID = int(getenv("OWNER_ID", default="OWNER_ID"))
 SUDO_USERS.append(OWNER_ID)
 
 
